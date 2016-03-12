931154fde715df77ff7078f2c48378c126e18cd1
hpothuri <harish.pvr@hotmail.com>
2016-03-12 15:50:12 +0530

    Custom SSO Split initial check in

---
+++ CustomSSOSplit/CustomSSOApp/ViewController/src/com/mdt/sso/view/LoginBean.java
@@ -0,0 +1,74 @@
+package com.mdt.sso.view;
+
+public class LoginBean {
+    public LoginBean() {
+        super();
+    }
+    
+    private String username;
+    private String password;
+    private String siteminderURL;
+    private String studyListURL;
+    private String clinicalTrainngURL;
+    private String forgetPasswordURL;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setSiteminderURL(String siteminderURL) {
+        this.siteminderURL = siteminderURL;
+    }
+
+    public String getSiteminderURL() {
+        if(siteminderURL==null)
+            siteminderURL=SSOUtils.getPropertyValue("fcc_url");
+        return siteminderURL;
+    }
+
+    public void setStudyListURL(String studyListURL) {
+        this.studyListURL = studyListURL;
+    }
+
+    public String getStudyListURL() {
+        if(studyListURL==null)
+            studyListURL=SSOUtils.getPropertyValue("target_url");
+        return studyListURL;
+    }
+    public void clear(){
+        System.out.println("Clear login details...");
+        this.username = null;
+        this.password = null;
+    }
+    public void setClinicalTrainngURL(String clinicalTrainngURL) {
+            this.clinicalTrainngURL = clinicalTrainngURL;
+    }
+
+    public String getClinicalTrainngURL() {
+        if(clinicalTrainngURL==null)
+            clinicalTrainngURL=SSOUtils.getPropertyValue("clinical_training_url");
+        return clinicalTrainngURL;
+    }
+
+    public void setForgetPasswordURL(String forgetPasswordURL) {
+        this.forgetPasswordURL = forgetPasswordURL;
+    }
+
+    public String getForgetPasswordURL() {
+        if(forgetPasswordURL==null)
+            forgetPasswordURL=SSOUtils.getPropertyValue("forget_password_url");
+        return forgetPasswordURL;
+    }
+}
---
+++ CustomSSOSplit/CustomSSOApp/ViewController/src/com/mdt/sso/view/SSOUtils.java
@@ -0,0 +1,142 @@
+package com.mdt.sso.view;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.sql.Connection;
+
+import java.sql.SQLException;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.naming.InitialContext;
+
+import javax.naming.NamingException;
+
+import javax.sql.DataSource;
+
+import oracle.adf.share.ADFContext;
+import oracle.adf.share.logging.ADFLogger;
+
+public class SSOUtils {
+    public SSOUtils() {
+        super();
+    }
+    private static Connection dbConnection;
+    // TODO get the databse url string from properties file
+    private final static String dbUrlString = "jdbc/MDTDS";
+//    public static final String SERVER_PROPERTY_FILE_LOCATION =
+//        "E:\\ECRS\\Custom Login\\RDC.git\\trunk\\CustomSSOApp\\ViewController";
+    public static final String SERVER_PROPERTY_FILE_LOCATION = System.getProperty("CUSTOMSSO_CONFIG");
+    public static final String PROPERTY_FILE_NAME = "ssourl.properties";
+    public static final String FILE_SEPERATOR = System.getProperty("file.separator");
+    public static final String SERVER_PROPERTY_FILE =
+        SERVER_PROPERTY_FILE_LOCATION + FILE_SEPERATOR + PROPERTY_FILE_NAME;
+    private static ADFLogger _logger = ADFLogger.createADFLogger(SSOUtils.class);
+
+    public static String getPropertyValue(String key) {
+        _logger.info("Start of AppConstants getEnvConstant");
+        String val = getConstantFromProperties(SERVER_PROPERTY_FILE, key, "CUSTOMSSO_CONSTANTS");
+        _logger.info("End of AppConstants getEnvConstant");
+        return val;
+    }
+
+    public static String getConstantFromProperties(String propFileName, String key, String sessionMap) {
+        _logger.info("AppConstants getConstantFromProperties Key :" + key);
+        ADFContext adfCtx = ADFContext.getCurrent();
+        Map sessionScope = adfCtx.getSessionScope();
+
+        //CHECK IF CONSTANT MAP EXIST IN SESSION SCOPE, IF NOT THEN POPULATE IT
+        if (sessionScope.get(sessionMap) == null) {
+
+            Properties propertiesBundle = new Properties();
+            try {
+                String propertyFilePath = propFileName;             
+                _logger.info("Property File location : " + propFileName);
+                if (propertyFilePath != null) {
+                    FileInputStream in = new FileInputStream(propertyFilePath);
+
+                    propertiesBundle.load(in);
+
+                    Enumeration em = propertiesBundle.keys();
+                    Map propertyMap = new HashMap();
+                    _logger.info("Loading properties into session scope map");
+                    while (em.hasMoreElements()) {
+                        String keyStr = em.nextElement().toString();
+                        propertyMap.put(keyStr, propertiesBundle.get(keyStr));
+                        _logger.info(keyStr + " : " + propertiesBundle.get(keyStr));
+                    }
+                    sessionScope.put(sessionMap, propertyMap);
+                }
+
+            } catch (IOException e) {
+                _logger.severe("Error getting Property file :" + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        
+        //READ KEY-VALUE PAIR FROM CONSTANT MAP
+
+        Map value = (Map)sessionScope.get(sessionMap);
+        if (value != null && value.containsKey(key)) {
+            _logger.info("AppConstants getConstantFromProperties : Value for contains Key " + key + " is " +
+                         value.get(key));
+            _logger.info("End of AppConstants getConstantFromProperties");
+            return (String)value.get(key);
+        } else {
+            _logger.info("AppConstants getConstantFromProperties : Value not contains Key " + key);
+            _logger.info("End of AppConstants getConstantFromProperties");
+            return null;
+        }
+    }
+
+    public static Connection getConnection() {
+        if (null != dbConnection) {
+            return dbConnection;
+        } else {
+            try {
+                InitialContext initialContext = new InitialContext();
+                DataSource ds = (DataSource)initialContext.lookup(dbUrlString);
+                dbConnection = ds.getConnection();
+            } catch (NamingException e) {
+                e.printStackTrace();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return dbConnection;
+    }
+
+//    private static Properties propertiesFromFile;
+//
+//    public static String getPropertyValue(String propName) {
+//        try {
+//            if (propertiesFromFile == null) {
+//                propertiesFromFile = new Properties();
+//                InputStream st =
+//                    Thread.currentThread().getContextClassLoader().getResourceAsStream("ssourl.properties");
+//                propertiesFromFile.load(st);
+//            }
+//
+//            if (propertiesFromFile != null) {
+//                return (String)propertiesFromFile.get(propName);
+//            } else
+//                return null;
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+
+    public static void main(String[] args) {
+        System.out.println(System.getenv("asl.log"));
+//        System.out.println(SSOUtils.getPropertyValue("fcc_url"));
+//        System.out.println(SSOUtils.getEnvConstant("fcc_url"));
+    }
+
+
+}
